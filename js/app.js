875202a7c840e2d86851b83b91be34e877f5bf58
Riazalmahmud <riazalmahmud041@gmail.com>
2021-08-21 19:03:12 +0600

    assinment-5

--- js/app.js
+++ js/app.js
@@ -13,18 +13,20 @@
 
 document.getElementById('memory-basic-8GB').addEventListener('click', function () {
 
+
+    // Extra Memory Cost
     const memory8GBMemory = document.getElementById('storage-cost');
     const memory8GBMemoryText = memory8GBMemory.innerText;
     const memory8GBMemoryNewPrice = parseInt(memory8GBMemoryText);
     const memory8GBMemoryTotal = memory8GBMemoryNewPrice + 0;
     memory8GBMemory.innerText = memory8GBMemoryTotal;
-
+    // Extra Memory total  
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
     const priviousMemoryPrice = parseFloat(totalPriceText);
     const totalProductPrice = priviousMemoryPrice + memory8GBMemoryTotal;
     totalPrice.innerText = totalProductPrice;
-
+    // Extra Memory total  Cost
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
     const updateNewPrice = parseInt(productTotalPriceText);
@@ -41,14 +43,14 @@ document.getElementById('memory-basic-8GB').addEventListener('click', function (
 
 document.getElementById('memory-Standard-16GB').addEventListener('click', function () {
 
-
+    // Extra Memory Standard Cost
     const memoryGBMemory = document.getElementById('memory-cost');
     const memory16GBMemoryText = memoryGBMemory.innerText;
     const memory16GBMemoryNewPrice = parseInt(memory16GBMemoryText);
     const memory16GBMemoryTotal = memory16GBMemoryNewPrice + 180;
     memoryGBMemory.innerText = memory16GBMemoryTotal;
 
-
+    // Extra Memory Standard total Price
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
     const priviousMemoryPrice = parseInt(totalPriceText);
@@ -57,7 +59,7 @@ document.getElementById('memory-Standard-16GB').addEventListener('click', functi
     console.log(totalProductPrice);
 
 
-
+    // Extra Memory Standard product Total Price
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
     const updateNewPrice = parseInt(productTotalPriceText);
@@ -70,10 +72,13 @@ document.getElementById('memory-Standard-16GB').addEventListener('click', functi
 
 
 document.getElementById('storage-basic-256GB').addEventListener('click', function () {
+
+    // Extra Storage basic Cost
     const storageCost = document.getElementById('storage-cost');
     const storageText = storageCost.innerText;
     const storageNewPrice = parseInt(storageText);
 
+    // Extra Storage basic total Price
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
     const priviousMemoryPrice = parseInt(totalPriceText);
@@ -81,7 +86,7 @@ document.getElementById('storage-basic-256GB').addEventListener('click', functio
     totalPrice.innerText = totalProductPrice;
     console.log(totalProductPrice);
 
-
+    // Extra Storage basic product Total Price
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
     const updateNewPrice = parseInt(productTotalPriceText);
@@ -90,12 +95,13 @@ document.getElementById('storage-basic-256GB').addEventListener('click', functio
 })
 document.getElementById('storage-Standerd-512GB').addEventListener('click', function () {
 
-
+    // Extra Storage Standerd Price
     const storage512GBCost = document.getElementById('storage-cost');
     const storage512GBText = storage512GBCost.innerText;
     const storage512GBNewPrice = parseInt(storage512GBText) + 100;
     storage512GBCost.innerText = storage512GBNewPrice;
 
+    // Extra Storage Standerd total Price
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
     const priviousMemoryPrice = parseInt(totalPriceText);
@@ -103,7 +109,7 @@ document.getElementById('storage-Standerd-512GB').addEventListener('click', func
     totalPrice.innerText = totalProductPrice;
     console.log(totalProductPrice);
 
-
+    // Extra Storage Standerd product total Price
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
     const updateNewPrice = parseInt(productTotalPriceText);
@@ -112,12 +118,13 @@ document.getElementById('storage-Standerd-512GB').addEventListener('click', func
 })
 document.getElementById('storage-premium-1TB').addEventListener('click', function () {
 
-
+    // Extra Storage premium Price
     const storage1TBCost = document.getElementById('storage-cost');
     const storage1TBText = storage1TBCost.innerText;
     const storage1TBNewPrice = parseInt(storage1TBText) + 180;
     storage1TBCost.innerText = storage1TBNewPrice;
 
+    // Extra Storage premium total Price
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
     const priviousMemoryPrice = parseInt(totalPriceText);
@@ -126,7 +133,7 @@ document.getElementById('storage-premium-1TB').addEventListener('click', functio
     console.log(totalProductPrice);
     console.log('get storage-premium-1TB')
 
-
+    // Extra Storage product Total Price
 
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
@@ -139,13 +146,15 @@ document.getElementById('storage-premium-1TB').addEventListener('click', functio
 
 document.getElementById('free-delivary').addEventListener('click', function () {
 
+    // free-delivary Price
+
     const delivaryCost = document.getElementById('delivery-cost');
     const delivaryCostText = delivaryCost.innerText;
     const newDelivery = parseInt(delivaryCostText);
     delivaryCost.innerText = newDelivery;
 
 
-
+    // free delivary total Price
 
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
@@ -154,7 +163,7 @@ document.getElementById('free-delivary').addEventListener('click', function () {
     totalPrice.innerText = totalProductPrice;
     console.log(totalProductPrice);
 
-
+    // free delivary product total Price
 
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
@@ -164,12 +173,14 @@ document.getElementById('free-delivary').addEventListener('click', function () {
 })
 document.getElementById('paid-delivery').addEventListener('click', function () {
 
+    // paid-delivary Price
     const paidDelivaryCost = document.getElementById('delivery-cost');
     const paidDelivaryCostText = paidDelivaryCost.innerText;
     const paidNewDelivery = parseInt(paidDelivaryCostText) + 20;
     paidDelivaryCost.innerText = paidNewDelivery;
     console.log('delivary cost 20', paidNewDelivery);
 
+    // paid-delivary total Price
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
     const priviousMemoryPrice = parseInt(totalPriceText);
@@ -178,7 +189,7 @@ document.getElementById('paid-delivery').addEventListener('click', function () {
     console.log(totalProductPrice);
 
 
-
+    // paid-delivary product total Price
     const productTotalPrice = document.getElementById('product-total-price')
     const productTotalPriceText = productTotalPrice.innerText;
     const updateNewPrice = parseInt(productTotalPriceText);
